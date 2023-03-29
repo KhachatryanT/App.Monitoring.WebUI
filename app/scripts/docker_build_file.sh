0228590bc99f823df6efbf9542339bdf83c786fe
Tigran Khachatrian <khachatriants@quantumart.ru>
2023-03-29 16:25:09 +0300

    #7 set production api links; mkdir ../publish in docker_build_file.sh

--- app/scripts/docker_build_file.sh
+++ app/scripts/docker_build_file.sh
@@ -1,3 +1,4 @@
-docker buildx build --platform linux/amd64 -f ../Dockerfile -t monitoring-webui:latest --load ../
+mkdir ../publish
+docker buildx build --platform linux/arm64/v8 -f ../Dockerfile -t monitoring-webui:latest --load ../
 docker save -o ../publish/monitoring-webui.tar monitoring-webui:latest
 docker image rm monitoring-webui:latest
--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    nodesApiUrl: 'http://monitoring-api',
-    webSocketUrl: 'http://monitoring-api',
-    longPollingIntervalInSec: 2,
+    nodesApiUrl: '/api',
+    webSocketUrl: '/api',
+    longPollingIntervalInSec: 30,
 };
