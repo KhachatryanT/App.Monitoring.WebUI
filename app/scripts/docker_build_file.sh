6be194064c0016cb7565e1835d6ab4a830e9fabb
Tigran Khachatrian <khachatriants@quantumart.ru>
2023-03-31 10:25:43 +0300

    #7 split docker build and save scripts

--- app/scripts/docker_build_file.sh
+++
@@ -1,4 +0,0 @@
-mkdir ../publish
-docker buildx build --platform linux/arm64/v8 -f ../Dockerfile -t monitoring-webui:latest --load ../
-docker save -o ../publish/monitoring-webui.tar monitoring-webui:latest
-docker image rm monitoring-webui:latest
